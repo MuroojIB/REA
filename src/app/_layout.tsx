f42dfec80edc0d5605d09469c14e0a0203b847dd
muroojIB <S444007269@uqu.edu.sa>
2026-08-15 10:06:05 +0300

    refactor(layout): update tabs navigation and route configurations

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,40 +1,42 @@
 import { Tabs } from 'expo-router';
+import { RequestsProvider } from '../features/requests/hooks/useRequests';
 
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: true }}>
-      
-      <Tabs.Screen
-        name="attendance/log"
-        options={{
-          title: 'Attendance Log',
-          tabBarLabel: 'Log',
-        }}
-        
-      />
-            <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarLabel: 'Home',
-        }}
-      />
-      
-      <Tabs.Screen
-        name="requests/index"
-        options={{
-          title: 'My Requests',
-          tabBarLabel: 'Requests',
-        }}
-      />
+    <RequestsProvider>
+      <Tabs screenOptions={{ headerShown: true }}>
+        <Tabs.Screen
+          name="attendance/log"
+          options={{
+            title: 'Attendance Log',
+            tabBarLabel: 'Log',
+          }}
+        />
 
-      <Tabs.Screen
-        name="requests/new"
-        options={{
-          title: 'New Request',
-          href: null, 
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarLabel: 'Home',
+          }}
+        />
+
+        <Tabs.Screen
+          name="requests/index"
+          options={{
+            title: 'My Requests',
+            tabBarLabel: 'Requests',
+          }}
+        />
+
+        <Tabs.Screen
+          name="requests/new"
+          options={{
+            title: 'New Request',
+            href: null, 
+          }}
+        />
+      </Tabs>
+    </RequestsProvider>
   );
 }
